9cffbe4453aa6859f6e6d15f6227cb31ad63c344
刘坤 <kun.liu@utsoft.cn>
2017-05-20 23:46:43 +0800

    spring 与 hibernate 集成

--- test/com/tz/Test02/Test02.java
+++ test/com/tz/Test02/Test02.java
@@ -12,24 +12,30 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import java.sql.SQLException;
 
 /**
- *  测试你配置的数据库链接是否成功
+ *  测试你配置的数据库链接是否成功 及 配置 文件里面配置的一些信息。
  */
 public class Test02 {
     public static void main(String[] args) throws SQLException {
+
+        // 测试你配置的数据库链接是否成功
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-
         //BasicDataSource dataSource = (BasicDataSource)context.getBean("dataSource");
-		//System.out.println(dataSource.getConnection());
-        //
-		//SessionFactory sessionFactory = (SessionFactory)context.getBean("sessionFactory");
-		//System.out.println(sessionFactory);
-//
+        //System.out.println(dataSource.getConnection());
 
 
-        System.out.println(context.getBean("sessionFactory"));
+        // 测试 SessionFactory 是否配置成功。
+        //ApplicationContext context = new AnnotationConfigApplicationContext("classpath:applicationContext.xml");
+        SessionFactory sessionFactory = (SessionFactory)context.getBean("sessionFactory");
+        System.out.println(sessionFactory);
+
 
 
         //UserDao userDao = (UserDao)context.getBean("userDao");
         //System.out.println(userDao);
+
+
+
+
+
     }
 }
